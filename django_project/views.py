3184af489022c4611b7f8c479227c411e49076f8
bibek977 <bhattaraibibek91@gmail.com>
2022-05-23 16:18:35 +0545

    text modifier completed

--- django_project/views.py
+++ django_project/views.py
@@ -7,3 +7,40 @@ def index(request):
 
 def about(request):
     return HttpResponse("This is a first project")
+
+def result(request):
+    
+    data = request.POST.get('text','default')
+    removepunc= request.POST.get('removepunc','off')
+    upper = request.POST.get('upper','off')
+    space = request.POST.get('space','off')
+
+    if removepunc == 'on':
+        analyzed = ''
+        punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for char in data:
+            if char not in punctuation:
+                analyzed = analyzed + char
+
+        dictData={"result" : "Result", "takeData" : analyzed}
+        data=analyzed
+
+    if upper == "on":
+        analyzed=''
+        for char in data:
+            analyzed = analyzed + char.upper()
+        dictData={"result" : "Result", "takeData" : analyzed}
+        data=analyzed
+
+    if space == 'on':
+        analyzed = ''
+        for index, char in enumerate(data):
+            if not (data[index] == ' ' and data[index + 1]== ' '):
+                analyzed = analyzed + char
+
+        dictData={"result" : "Result", "takeData" : analyzed}
+
+    if (removepunc != 'on' and upper != "on" and space != 'on'):
+        return HttpResponse("nothing clicked")
+    return render(request, "result.html", dictData)
+    
